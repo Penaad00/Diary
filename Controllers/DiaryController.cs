dcd14aa5d277ee7ec1201ea622c04e98d95996a1
Penaad00 <adam.penaz01@upol.cz>
2025-06-29 18:57:58 +0200

    Style changes and form page added

---
+++ Controllers/DiaryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Diary.Controllers
+{
+    public class DiaryController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
