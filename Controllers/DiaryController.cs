8b39769bde33bfaee9c8ed38e8d241fb89531e20
Penaad00 <adam.penaz01@upol.cz>
2025-06-30 20:48:44 +0200

    diary: change records and remove records

--- Controllers/DiaryController.cs
+++ Controllers/DiaryController.cs
@@ -22,6 +22,16 @@ namespace Diary.Controllers
             return View();
         }
 
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Search()
+        {
+            return View();
+        }
+
         [HttpPost]
         public async Task<IActionResult> Add(DiaryEntry model)
         {
@@ -40,5 +50,47 @@ namespace Diary.Controllers
                 return View(model);
             }
         }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            var diaryEntry = await _dbContext.DiaryEntries.FindAsync(id);
+
+            var currentUserId = _userManager.GetUserId(User);
+
+            return View(diaryEntry);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveChanges(DiaryEntry model)
+        {
+            _dbContext.Update(model);
+            await _dbContext.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+            
+        }
+
+        public async Task<IActionResult> Remove(int? id)
+        {
+            var diaryEntry = await _dbContext.DiaryEntries.FindAsync(id);
+
+            if (diaryEntry == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            _dbContext.DiaryEntries.Remove(diaryEntry);
+            await _dbContext.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SearchUser(string searchUser)
+        {
+            
+            return RedirectToAction("Index", "Home");
+        }
+
+
     }
 }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -40,5 +40,6 @@ namespace Diary.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
